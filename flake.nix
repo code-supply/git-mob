55195ccccd20b2dc189ea8ff7e1c3ae7e1a0d6eb
Andrew Bruce <me@andrewbruce.net>
2023-07-17 21:14:45 +0100

    Separate mob db from coauthors db

--- flake.nix
+++ flake.nix
@@ -44,6 +44,7 @@
               export PATH="$PATH:$out/bin"
               export GIT_MOB_COAUTHORS=git-coauthors
               export GIT_MOB_TEMPLATE=gitmessage.txt
+              export GIT_MOB_LIST=git-mob-list
               patchShebangs test/git-mob-tests
               test/git-mob-tests
             '';
