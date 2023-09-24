a1edd99caf5af22d9bfbb9f6844cc255b172f134
Andrew Bruce <me@andrewbruce.net>
2023-09-24 11:10:21 +0100

    Whitespace / getExe

--- flake.nix
+++ flake.nix
@@ -6,62 +6,55 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-      writeScript = with pkgs;
-        substituteAll {
-          src = ./src/write;
-          isExecutable = true;
-          jq = "${jq}/bin/jq";
-        };
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
 
-      gitMobPrintScript = with pkgs;
-        substituteAll {
-          src = ./src/git-mob-print;
-          isExecutable = true;
-          jq = "${jq}/bin/jq";
-        };
+        writeScript = with pkgs;
+          substituteAll {
+            src = ./src/write;
+            isExecutable = true;
+            jq = lib.getExe jq;
+          };
 
-      gitMob = with pkgs; (
-        stdenv.mkDerivation
-          {
-            name = "git-mob";
-            src = ./.;
-            installPhase = ''
-              install -Dm755 ${./src/git-mob} $out/bin/git-mob
-              install -Dm755 ${./src/git-solo} $out/bin/git-solo
-              install -Dm755 ${writeScript} $out/bin/write
-              install -Dm755 ${gitMobPrintScript} $out/bin/git-mob-print
-            '';
-            doInstallCheck = true;
-            installCheckPhase = ''
-              export PATH="$PATH:$out/bin"
-              export GIT_MOB_COAUTHORS=git-coauthors
-              export GIT_MOB_TEMPLATE=gitmessage.txt
-              export GIT_MOB_LIST=git-mob-list
-              patchShebangs test/git-mob-tests
-              test/git-mob-tests
-            '';
-          }
-      );
-    in
-    {
-      formatter = pkgs.nixpkgs-fmt;
-      apps.default = flake-utils.lib.mkApp { drv = gitMob; };
-      packages.default = gitMob;
-      devShells.default = pkgs.mkShell {
-        packages = [
-          gitMob
-          pkgs.nixpkgs-fmt
-          pkgs.shellcheck
-        ];
-      };
-    }
-    );
+        gitMobPrintScript = with pkgs;
+          substituteAll {
+            src = ./src/git-mob-print;
+            isExecutable = true;
+            jq = lib.getExe jq;
+          };
+
+        gitMob = pkgs.stdenv.mkDerivation {
+          name = "git-mob";
+          src = ./.;
+          installPhase = ''
+            install -Dm755 ${./src/git-mob} $out/bin/git-mob
+            install -Dm755 ${./src/git-solo} $out/bin/git-solo
+            install -Dm755 ${writeScript} $out/bin/write
+            install -Dm755 ${gitMobPrintScript} $out/bin/git-mob-print
+          '';
+          doInstallCheck = true;
+          installCheckPhase = ''
+            export PATH="$PATH:$out/bin"
+            export GIT_MOB_COAUTHORS=git-coauthors
+            export GIT_MOB_TEMPLATE=gitmessage.txt
+            export GIT_MOB_LIST=git-mob-list
+            patchShebangs test/git-mob-tests
+            test/git-mob-tests
+          '';
+        };
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+        apps.default = flake-utils.lib.mkApp { drv = gitMob; };
+        packages.default = gitMob;
+        devShells.default = pkgs.mkShell {
+          packages = [
+            gitMob
+            pkgs.nixpkgs-fmt
+            pkgs.shellcheck
+          ];
+        };
+      });
 }
