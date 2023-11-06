bee7ed4664364a1f4c56f0a8d4056ad1404861ca
Andrew Bruce <me@andrewbruce.net>
2023-11-06 21:35:31 +0000

    Eliminate flake-utils

--- flake.nix
+++ flake.nix
@@ -2,33 +2,53 @@
   description = "A git-mob implementation in bash and jq";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/master";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-
-        callPackage = pkgs.lib.callPackageWith (pkgs // {
-          version =
-            if self ? rev
-            then self.rev
-            else "dirty";
+  outputs = { self, nixpkgs }:
+    let
+      forAllSystems = generate: nixpkgs.lib.genAttrs [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ]
+        (system: generate (
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+            callPackage = pkgs.lib.callPackageWith
+              (pkgs // {
+                version =
+                  if self ? rev
+                  then self.rev
+                  else "dirty";
+              });
+          in
+          {
+            inherit pkgs;
+            gitMob = callPackage ./default.nix { };
+          }
+        ));
+    in
+    {
+      formatter = forAllSystems ({ pkgs, ... }: pkgs.nixpkgs-fmt);
+      apps = forAllSystems
+        ({ gitMob }: {
+          default = {
+            type = "app";
+            program = gitMob;
+          };
         });
-
-        gitMob = callPackage ./default.nix { };
-      in
-      {
-        formatter = pkgs.nixpkgs-fmt;
-        apps.default = flake-utils.lib.mkApp { drv = gitMob; };
-        packages.default = gitMob;
-        devShells.default = pkgs.mkShell {
+      packages = forAllSystems ({ gitMob, ... }: {
+        default = gitMob;
+      });
+      devShells = forAllSystems ({ pkgs, gitMob, ... }: {
+        default = pkgs.mkShell {
           packages = [
             gitMob
             pkgs.nixpkgs-fmt
           ];
         };
       });
+    };
 }
